56ea1bd86f481fcbde3fc61d2005d68d43d08e6a
OdsQuiz <133040488+OdsQuiz@users.noreply.github.com>
2023-05-12 15:55:34 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://hack2030-backend.onrender.com/'
+    baseURL: 'https://hack2030-backend-ptux.onrender.com/'
     // baseURL: 'http://localhost:5000'
 })
 
